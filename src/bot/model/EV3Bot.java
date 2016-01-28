2a9543a980ee9965d94a713b3adc8200b61dab34
brandonjolley97 <brandonjolley97@gmail.com>
2016-01-28 08:48:45 -0700

    added some imports and filled some methods

--- src/bot/model/EV3Bot.java
+++ src/bot/model/EV3Bot.java
@@ -1,8 +1,16 @@
 package bot.model;
 
+import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.LCD;
+import lejos.hardware.motor.Motor;
+import lejos.hardware.port.port;
+import lejos.hardware.chassis.Wheel;
+import lejos.hardware.chassis.WheelChassis;
 import lejos.robotics.navigation.MovePilot;
 import lejos.utility.Delay;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+
 
 
 public class EV3Bot 
@@ -13,31 +21,47 @@ public class EV3Bot
 	private long waitTime;
 	
 	private MovePilot botPilot;
+	private EV3UltrasonicSensor distanceSensor;
+	private EV3TouchSensor backTouch;
 	
 	public EV3Bot()
 	{
 		this.botMessage = "Brandon codes BrandonBot";
-		this.xPosition = 
+		this.xPosition = 50;
+		this.yPosition = 50;
+		this.waitTime = 4000;
+		
+		distanceSensor = 
 	}
 	
 	private void setupPilot()
 	{
+		//
+		//
+		//
+		//
+		
 		
 	}
 	
 	public void driveRoom()
 	{
-		
+		//call private helper method here
+		displayMessage("driveRoom");
 	}
 	
 	private void dislayMessage()
 	{
+		LCD.drawString(botMessage, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+		
 		
 	}
 	
 	private void displayMessage(String message)
 	{
-		LCD.drawString
+		LCD.drawString(message, xPosition, yPosition);
+		LCD.msDelay(waitTime);
 	}
 	
 }
